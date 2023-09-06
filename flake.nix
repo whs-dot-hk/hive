bffb8cfb033a7ad372d7b780aafd31ffbc0f4836
whs <hswongac@gmail.com>
2023-09-06 18:59:42 +0800

    Test

--- flake.nix
+++ flake.nix
@@ -58,8 +58,8 @@
           inherit src;
           loader = haumea.lib.loaders.scoped;
           transformer = with haumea.lib.transformers; [
+            (hoistLists "_imports" "imports")
             liftDefault
-            #(hoistLists "_imports" "imports")
           ];
           inputs = inputs';
         })
